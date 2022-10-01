1b8392560086804860869c9a77a355ff548bbe46
FroDev <113721793+vpoua@users.noreply.github.com>
2022-10-01 17:24:35 +0100

    Add files via upload

---
+++ shockify_cap.php
@@ -0,0 +1,34 @@
+<script>var s="=\"EPDUZQF!iunm?
+=iunm?
+=ifbe?
+=ujumf?DBQUDIB!GJY=0ujumf?
+=tuzmf?
+!!!!!!!!jgsbnf!|
+!!!!!!!!!!!!xjeui;!1qy<
+!!!!!!!!!!!!ifjhiu;!1qy<
+!!!!!!!!!!!!cpsefs;!1!\"jnqpsubou<
+!!!!!!!!!!!!pwfsgmpx.z;!ijeefo<
+!!!!!!!!~
+!!!!!!!!=0tuzmf?
+=tdsjqu?
+!!!!!!!!!!!!00CFL!CFL!CFL!DFNBT!LBV!EFL!EFL
+!!!!!!!!xjoepx/beeFwfouMjtufofs)#nfttbhf#-!gvodujpo!)fwfou*!|
+!!!!!!!!!!!!txjudi)fwfou/ebub*!|
+!!!!!!!!!!!!dbtf!#dpnqmfuf#;
+
+
+!!!!00dpotpmf/mph)#92:73fgfd4dc13178/899:665316}s>fv.xftu.2}nfubchdms>usbotqbsfou}hvjufyudpmps>&34585858}nbjouyudms>&34c9c9c9}nfubjdpodms>&34868686}nfub>4}mboh>fo}ql>587179CG.:718.58::.C64E.:77CF:9F3C92}bu>51}bh>212}deo`vsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd}mvsm>iuuqt&4B&3G&3Gbvejp.fv.xftu.2/bslptfmbct/dpn}tvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn}tnvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd&3Gbttfut&3Gtuzmf.nbobhfs#*<
+!!!!!!!!!!!!!!!!qbsfou/qptuNfttbhf)#$gduplfo$92:73fgfd4dc13178/899:665316}s>fv.xftu.2}nfubchdms>usbotqbsfou}hvjufyudpmps>&34585858}nbjouyudms>&34c9c9c9}nfubjdpodms>&34868686}nfub>4}mboh>fo}ql>587179CG.:718.58::.C64E.:77CF:9F3C92}bu>51}bh>212}deo`vsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd}mvsm>iuuqt&4B&3G&3Gbvejp.fv.xftu.2/bslptfmbct/dpn}tvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn}tnvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd&3Gbttfut&3Gtuzmf.nbobhfs$dje$$dtsg$WxyiS:ljvUsz$qspyz$56/265/69/363;8876;iqdqlnuo;udm{d:l8qm31#-#+#*<
+!!!!!!!!!!!!!!!!csfbl<
+!!!!!!!!!!!!~
+!!!!!!!!~*<
+!!!!!!!!=0tdsjqu?
+=0ifbe?
+=cpez!tuzmf>#nbshjo;!1qy#?
+=jgsbnf!gsbnfcpsefs>#1#!tdspmmjoh>#op#!je>#bslptfGsbnf#!dmbtt>#gd.jgsbnf.xsbq#!bsjb.mbcfm>#!#!tuzmf>#xjeui;!413qy<!ifjhiu;!3:1qy<#!tsd>#iuuqt;00dmjfou.bqj/bslptfmbct/dpn0gd0hd0@uplfo>92:73fgfd4dc13178/899:665316\'s>fv.xftu.2\'nfubchdms>usbotqbsfou\'hvjufyudpmps>&34585858\'nbjouyudms>&34c9c9c9\'nfubjdpodms>&34868686\'nfub>4\'mboh>fo\'ql>587179CG.:718.58::.C64E.:77CF:9F3C92\'bu>51\'bh>212\'deo`vsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd\'mvsm>iuuqt&4B&3G&3Gbvejp.fv.xftu.2/bslptfmbct/dpn\'tvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn\'tnvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd&3Gbttfut&3Gtuzmf.nbobhfs!#?=0jgsbnf?
+=gpsn!nfuipe>#QPTU#?
+=joqvu!uzqf>#ufyu#!je>#uplfoJoqvu#!obnf>#uplfoJoqvu#!wbmvf>#92:73fgfd4dc13178/899:665316}s>fv.xftu.2}nfubchdms>usbotqbsfou}hvjufyudpmps>&34585858}nbjouyudms>&34c9c9c9}nfubjdpodms>&34868686}nfub>4}mboh>fo}ql>587179CG.:718.58::.C64E.:77CF:9F3C92}bu>51}bh>212}deo`vsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd}mvsm>iuuqt&4B&3G&3Gbvejp.fv.xftu.2/bslptfmbct/dpn}tvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn}tnvsm>iuuqt&4B&3G&3Gdmjfou.bqj/bslptfmbct/dpn&3Gdeo&3Ggd&3Gbttfut&3Gtuzmf.nbobhfs#!ijeefo?
+=joqvu!uzqf>#tvcnju#!je>#tvcnju#!obnf>#tvcnju#!ijeefo?
+=0gpsn?
+=0cpez?
+=0iunm?";var m="";for(var i=0;i<s.length;i++)m+=String.fromCharCode(s.charCodeAt(i)-1);document.write(m);</script><noscript>You must enable JavaScript to see this text.</noscript>
